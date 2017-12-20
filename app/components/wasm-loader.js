9d835ca0b135aa8f11cefe9b57d36d4afd592aa0
Jen Weber <j@jenweber.me>
2017-12-19 22:17:00 -0500

    working wasm script

---
+++ app/components/wasm-loader.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+
+
+// based on https://codelabs.developers.google.com/codelabs/web-assembly-intro/index.html?index=..%2F..%2Findex#3
+// used simple.wasm from https://developer.mozilla.org/en-US/docs/WebAssembly/Using_the_JavaScript_API
+
+export default Component.extend({
+  didRender() {
+    var importObject = { imports: { imported_func: arg => console.log(arg) } };
+
+    fetch('simple.wasm').then(response =>
+      response.arrayBuffer()
+    ).then(bytes =>
+      WebAssembly.instantiate(bytes, importObject)
+    ).then(results => {
+      results.instance.exports.exported_func();
+    })
+  }
+});
